a9be73304b80346da1cd3d2fab3cf0eae9a77acf
Enzo Reis Francelli <francelli.dev@gmail.com>
2026-01-27 16:12:48 -0300

    Add files via upload

---
+++ Linguagem C#/Projeto_menor3_csharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_menor3_csharp {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int x, y, z;
+
+            Console.Write("Digite o primeiro valor: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo valor: ");
+            y = int.Parse(Console.ReadLine());
+            Console.Write("Digite o terceiro valor: ");
+            z = int.Parse(Console.ReadLine());
+
+            Console.Write("\n");
+            if (x <= y && x <= z) {
+                Console.Write("O MENOR VALOR EH " + x);
+            } else if (y <= x && y <= z) {
+                Console.Write("O MENOR VALOR EH " + y);
+            } else if (z <= x && z <= y) {
+                Console.Write("O MENOR VALOR EH " + z);
+            }
+
+
+        }
+    }
+}
